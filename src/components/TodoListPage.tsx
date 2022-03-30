9a73dce4ca08b74cfa2f67359ac4fb3722447b6a
songiwon <biob1121@naver.com>
2022-03-30 23:19:17 +0900

    docs:  FixFix

--- src/components/TodoListPage.tsx
+++ src/components/TodoListPage.tsx
@@ -9,12 +9,30 @@ const TodoList = () => {
   const [UpdateSwitch, setUpdateSwitch] = useState(false);
   const [listState, setListState] = useRecoilState(list);
   const [text, setText] = useState("");
+  const [ChangeText, setChangeText] = useState("");
 
   const PlusList = () => {
-    let TestListState: any = [...listState];
-    TestListState.push(text);
-    setListState(TestListState);
-    setText("");
+    if (text != "") {
+      let TestListState: any = [...listState];
+      TestListState.push(text);
+      setListState(TestListState);
+      setText("");
+    }
+  };
+
+  const UpdateList3 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChangeText(e.target.value);
+  };
+
+  const UpdateList2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let ListIndex: any = e.target.name;
+    const ListMap = listState.map((x, index) => {
+      if (index == ListIndex) {
+        CopyListState = [...listState];
+        CopyListState[ListIndex] = e.target.value;
+        return x;
+      }
+    });
   };
 
   const UpdateList = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,6 +45,14 @@ const TodoList = () => {
     CopyListState[ListIndex] = e.target.value;
   };
 
+  const UpdateListCheck2 = (e: any) => {
+    const num = e.target.name;
+    CopyListState = [...listState];
+    CopyListState[num] = ChangeText;
+    setListState(CopyListState);
+    console.log(listState)
+  };
+
   const UpdateListCheck = () => {
     num = 0;
     setListState(CopyListState);
@@ -36,7 +62,7 @@ const TodoList = () => {
     setText(e.target.value);
   };
 
-  const DeleteLIstIndex = (e: any) => {
+  const DeleteListIndex = (e: any) => {
     let ListIndex: number = e.target.name;
     const ListFilter = listState.filter((x, index) => {
       if (index != ListIndex) {
@@ -48,15 +74,15 @@ const TodoList = () => {
 
   const listStateMap = listState.map((x, index) => {
     return (
-      <S.listElement name={index}>
+      <S.listElement key={index} name={index}>
         {x}
-        <S.DeleteButton name={index} onClick={DeleteLIstIndex}>
+        <S.DeleteButton name={index} onClick={DeleteListIndex}>
           삭제
         </S.DeleteButton>
         {UpdateSwitch ? (
           <>
-            <S.UpdateInput name={index} onChange={UpdateList}/>
-            <S.UpdateCheck onClick={UpdateListCheck}>확인</S.UpdateCheck>
+            <S.UpdateInput name={index} onChange={UpdateList3} />
+            <S.UpdateCheck onClick={UpdateListCheck2}>확인</S.UpdateCheck>
             <S.UpdateBack
               onClick={() => {
                 setUpdateSwitch(false);
@@ -85,7 +111,39 @@ const TodoList = () => {
         <S.inputBox onChange={onChangeText} value={text} />
         <S.PlusButton onClick={PlusList}>추가</S.PlusButton>
       </S.TextBox>
-      <S.UnNumberList>{listStateMap}</S.UnNumberList>
+      <S.UnNumberList>
+        {listState.map((x, index) => {
+          return (
+            <S.listElement key={index} name={index}>
+              {x}
+              <S.DeleteButton name={index} onClick={DeleteListIndex}>
+                삭제
+              </S.DeleteButton>
+              {UpdateSwitch ? (
+                <>
+                  <S.UpdateInput name={index} onChange={UpdateList3} />
+                  <S.UpdateCheck onClick={UpdateListCheck2}>확인</S.UpdateCheck>
+                  <S.UpdateBack
+                    onClick={() => {
+                      setUpdateSwitch(false);
+                    }}
+                  >
+                    취소
+                  </S.UpdateBack>
+                </>
+              ) : (
+                <S.UpdateButton
+                  onClick={() => {
+                    setUpdateSwitch(true);
+                  }}
+                >
+                  수정
+                </S.UpdateButton>
+              )}
+            </S.listElement>
+          );
+        })}
+      </S.UnNumberList>
     </S.MainPage>
   );
 };
